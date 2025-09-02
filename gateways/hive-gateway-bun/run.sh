9616fa879bae4668e1023de26f60a092dabcb68c
Arda TANRIKULU <ardatanrikulu@gmail.com>
2025-09-02 18:16:39 +0300

    Try again

--- gateways/hive-gateway-bun/run.sh
+++ gateways/hive-gateway-bun/run.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-NODE_ENV=production ~/.bun/bin/bun hive-gateway supergraph supergraph.graphql --port 4000 --jit
+NODE_ENV=production ~/.bun/bin/bun hive-gateway supergraph supergraph.graphql --host 0.0.0.0 --port 4000 --jit
