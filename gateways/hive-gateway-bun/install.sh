0dd3c145c09d23e7835bf4084bf9c31712220747
Arda TANRIKULU <ardatanrikulu@gmail.com>
2025-09-02 18:07:05 +0300

    Run bun

--- gateways/hive-gateway-bun/install.sh
+++ gateways/hive-gateway-bun/install.sh
@@ -1,3 +1,2 @@
-curl -fsSL https://bun.com/install | bash # for macOS, Linux, and WSL
-source /home/runner/.bashrc
-bun install
+curl -fsSL https://bun.com/install | bash # for macOS, Linux, and WSLx
+~/.bun/bin install
--- gateways/hive-gateway-bun/run.sh
+++ gateways/hive-gateway-bun/run.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-NODE_ENV=production bun hive-gateway supergraph supergraph.graphql --port 4000 --jit
+NODE_ENV=production ~/.bun/bin hive-gateway supergraph supergraph.graphql --port 4000 --jit
