5fb22b4c35b17582e2f3fc8ad107aae726fb9a53
Arda TANRIKULU <ardatanrikulu@gmail.com>
2025-09-03 17:56:44 +0300

    Try

--- gateways/hive-gateway-bun/run.sh
+++ gateways/hive-gateway-bun/run.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-NODE_ENV=production ~/.bun/bin/bun hive-gateway supergraph supergraph.graphql --host 0.0.0.0 --port 4000 --jit > ./gateway_log.txt 2>&1
+NODE_ENV=production ~/.bun/bin/bun hive-gateway supergraph supergraph.graphql --jit > ./gateway_log.txt 2>&1
