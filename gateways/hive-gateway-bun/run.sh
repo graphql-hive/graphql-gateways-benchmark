4b3a2e30f30e78f61d840c8538077d227100ce68
Arda TANRIKULU <ardatanrikulu@gmail.com>
2025-09-03 19:07:00 +0300

    Lg

--- gateways/hive-gateway-bun/run.sh
+++ gateways/hive-gateway-bun/run.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-NODE_ENV=production ~/.bun/bin/bun hive-gateway supergraph supergraph.graphql --jit > ./gateway_log.txt 2>&1
+NODE_ENV=production npx bun hive-gateway supergraph supergraph.graphql --jit > ./gateway_log.txt 2>&1
