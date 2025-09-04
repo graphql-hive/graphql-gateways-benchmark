93479429acaf405bc45c4b4ff22d8f6ccb7ed0f6
Dotan Simha <dotansimha@gmail.com>
2025-09-04 10:40:59 +0300

    latest hive-router, fix screenshots again

--- gateways/hive-router/install.sh
+++ gateways/hive-router/install.sh
@@ -1,1 +1,1 @@
-curl -o- https://raw.githubusercontent.com/graphql-hive/router/main/install.sh | sh -s v0.0.5
+curl -o- https://raw.githubusercontent.com/graphql-hive/router/main/install.sh | sh -s v0.0.8
