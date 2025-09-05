5130d05598929e7e74d6291d2065db97d6d5f385
Arda TANRIKULU <ardatanrikulu@gmail.com>
2025-09-05 03:30:04 +0300

    Add files

--- gateways/hive-gateway/gateway.config.ts
+++ gateways/hive-gateway/gateway.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from '@graphql-hive/gateway'
 
+// Disable limits as we have a huge query
 export const gatewayConfig = defineConfig({
     maxDepth: false,
     maxTokens: false,
